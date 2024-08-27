65acbde42b187fd1751764726ce452d5543b14f7
likith1003 <123715227+likith1003@users.noreply.github.com>
2024-08-27 12:16:39 +0530

    Add files via upload

---
+++ bitter/app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('register', register, name='register'),
+    path('user_login', user_login, name='user_login'),
+    path('user_logout', user_logout, name='user_logout'),
+    path('create_tweet', create_tweet, name='create_tweet'),
+    path('update<pk>', update, name='update')
+]
